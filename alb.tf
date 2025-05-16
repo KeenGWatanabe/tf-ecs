c7fea5e9cc23f91596da73b47994f768fda1f0f6
KeenGWatanabe <mckeenweb@gmail.com>
2025-05-16 22:21:25 +0800

    check cpu

--- alb.tf
+++ alb.tf
@@ -36,14 +36,7 @@ resource "aws_lb_listener" "app" {
   port              = 80
   protocol          = "HTTP"
   
-  # default_action {
-  #   type = "redirect"
-  #   redirect {
-  #     port        = "443"
-  #     protocol    = "HTTPS"
-  #     status_code = "HTTP_301"
-  #   }
-  # }
+ 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app.arn
--- main.tf
+++ main.tf
@@ -55,8 +55,8 @@ resource "aws_ecs_task_definition" "app" {
   family                   = "nodejs-app-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = 1024
-  memory                   = 2048
+  cpu                      = 512 #1024 
+  memory                   = 1024 #2048
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([{
